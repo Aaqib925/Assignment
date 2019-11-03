30a3abc6a817e3cc36cc237e181486d17be02833
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-03 23:55:41 +0500

    Assignment 1

---
+++ assignment.py
@@ -0,0 +1,41 @@
+# question no.1
+
+print("""
+    Twinkle, twinkle, little star,
+        How i wonder what you are!
+            Up above the World so high,
+            like a diamond on the sky,
+    Twinkle, twinkle, little star,
+        how i wonder what you are
+""")
+
+# Question no. 2
+import sys
+print(sys.version)
+
+# question no. 3
+import datetime
+now = datetime.datetime.now()
+print("Current date and time: ")
+print(str(now))
+
+
+# question no. 4
+radius = int(input("Enter the radius of the circle: "))
+
+area = (22/7)*(radius * radius)
+
+print(area)
+
+# question no. 5
+
+first_name = input("Enter first name: ")
+last_name = input("Enter last name: ")
+print(last_name, first_name)
+
+# question no. 6
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("The sum of x and y is: ", x+y)
