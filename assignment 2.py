1e5974a8a68056513f4adb0af2cdd78eb3e349bc
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-12 21:03:10 +0500

    Assignment 2

--- assignment 2.py
+++ assignment 2.py
@@ -5,58 +5,58 @@ Program to calculate the grade....and takes five inputs of marks of each subject
 
 """
 
-# chem = int(input("Enter marks in Chemistry: "))
-# phy = int(input("Enter marks in Physics: "))
-# math = int(input("Enter marks in Maths: "))
-# eng = int(input("Enter marks in English: "))
-# urdu = int(input("Enter marks in Urdu: "))
-#
-# total = ((chem + phy + math + eng + urdu) / 500) * 100
-# print("Your percentage is: ", total)
-#
-# if total <= 100 and total >= 80:
-#     print("Your grade is: A1")
-#
-# elif total < 80 and total >= 70:
-#     print("Your grade is: A")
-#
-# elif total < 70 and total >= 60:
-#     print("Your grade is: B")
-#
-# elif total < 60 and total >= 50:
-#     print("Your grade is C")
-#
-# else:
-#     print("Your grade is: F")
+chem = int(input("Enter marks in Chemistry: "))
+phy = int(input("Enter marks in Physics: "))
+math = int(input("Enter marks in Maths: "))
+eng = int(input("Enter marks in English: "))
+urdu = int(input("Enter marks in Urdu: "))
 
-# Question no. 2
+total = ((chem + phy + math + eng + urdu) / 500) * 100
+print("Your percentage is: ", total)
 
-# num = int(input("Enter any number: "))
-#
-# if num % 2 == 0:
-#     print("The number is even")
-# else:
-#     print("The number is odd")
+if total <= 100 and total >= 80:
+    print("Your grade is: A1")
 
-# Question no. 3
+elif total < 80 and total >= 70:
+    print("Your grade is: A")
 
-# my_list = [1, 2, 3, 5, 8, 9, 10, 11, 14, 17, 19]
-# print(len(my_list))
+elif total < 70 and total >= 60:
+    print("Your grade is: B")
+
+elif total < 60 and total >= 50:
+    print("Your grade is C")
+
+else:
+    print("Your grade is: F")
+
+Question no. 2
+
+num = int(input("Enter any number: "))
+
+if num % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+Question no. 3
+
+my_list = [1, 2, 3, 5, 8, 9, 10, 11, 14, 17, 19]
+print(len(my_list))
 
 
-# Question no. 4
+Question no. 4
 
-# my_list = [1, 2, 3, 5, 8, 9, 10, 11, 14, 17, 19]
-# print(sum(my_list))
+my_list = [1, 2, 3, 5, 8, 9, 10, 11, 14, 17, 19]
+print(sum(my_list))
 
-# Question no. 5
-# my_list = [1, 2, 3, 5, 8, 9, 10, 11, 14, 17, 19]
-# print(max(my_list))
+Question no. 5
+my_list = [1, 2, 3, 5, 8, 9, 10, 11, 14, 17, 19]
+print(max(my_list))
 
-# Question no. 6
-# my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-#
-# for i in my_list:
-#     if i < 5:
-#         print(i)
+Question no. 6
+my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for i in my_list:
+    if i < 5:
+        print(i)
 
