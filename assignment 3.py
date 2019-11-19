82de7361d24fd8dce619b2d9c7389fe3c9c77e66
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-20 01:05:25 +0500

    Assignment 2

---
+++ assignment 3.py
@@ -0,0 +1,66 @@
+# question no. 1
+
+""""
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("1. Addition.")
+print("2. Subtraction.")
+print("3. Multiplication.")
+print("4. division.")
+print("5. Power.")
+while True:
+    choice = int(input("Enter the number for which operation you want to do: "))
+
+    if choice == 1:
+        print(x, "+", y, "=", x + y)
+
+    elif choice == 2:
+        print(x, "-", y, "=", x - y)
+
+    elif choice == 3:
+        print(x, "*", y, "=", x * y)
+
+    elif choice == 4:
+        print(x, "/", y, "=", x / y)
+
+    elif choice == 5:
+        print(pow(x, y))
+
+    else:
+        print("Please select correct choice.")
+
+"""
+
+# question no. 2
+
+"""
+my_list = [1, 2, 3, 4, 5]
+
+x = int(input("Which element you want to find in this list? "))
+for i in my_list:
+    if i == x:
+        print("The element exist.")
+
+"""
+
+# question no. 3
+"""
+dictionary = {"name": "Aaqib", "father name": "Nazir", }
+
+dictionary["Roll_number"] = "CT-47"
+print(dictionary)
+
+"""
+
+# question no. 4
+
+"""
+sum_ = 0
+dictionary = {"a": 1, "b": 2, "c": 3}
+for i in dictionary:
+    sum_ += dictionary[i]
+print(sum_)
+
+"""
+
