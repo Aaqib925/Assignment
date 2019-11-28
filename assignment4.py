53f6ca476f10b5cc6a1c10ffdca569ac7eaa5427
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-28 18:49:20 +0500

    Assignment 3

---
+++ assignment4.py
@@ -0,0 +1,14 @@
+# question no. 1
+
+# my_dict = {"first_name": "Aaqib", "last_name": "Nazir", "age": 18, "city": "Karachi"}
+#
+# # print(my_dict.items())
+#
+# my_dict["qualification"] = "Intermediate"
+#
+# my_dict.update({"qualification": "Higher education"})
+#
+# del my_dict["qualification"]
+# print(my_dict)
+
+# question no. 2
