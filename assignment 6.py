1de561fb614d956d1ce3342c38f17681d5ae087f
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-20 13:52:35 +0500

    assignment 6

--- assignment 6.py
+++ assignment 6.py
@@ -3,3 +3,11 @@
 
 # Object oriented programming is the characteristic of the programming language in which we can write our code by
 # considering it as an object. Some of the object oriented programming languages are Python, Java, C++, C# and others
+
+# Question no. 2
+# List down the Benefits of OOP?
+
+# 1- It provide a the definite structure in which the implementation details are hidden.
+# 2- It makes the software or any program easy to maintain.
+# 3- It increases the skills of the developer to think more by considering a object
+# 4 - We can reuse of code through inheritance
