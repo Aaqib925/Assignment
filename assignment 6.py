0a60cad5a9350ee6ac9e3525aabf307736df8f87
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-20 13:47:00 +0500

    assignment 5

--- assignment 6.py
+++ assignment 6.py
@@ -1,1 +1,5 @@
 # Question no.1
+# Define Object Oriented Programming Language?
+
+# Object oriented programming is the characteristic of the programming language in which we can write our code by
+# considering it as an object. Some of the object oriented programming languages are Python, Java, C++, C# and others
