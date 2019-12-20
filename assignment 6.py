a402fd6dfc580368ef4c862ab148c8ef1cb4e3df
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-20 13:55:47 +0500

    assignment 6

--- assignment 6.py
+++ assignment 6.py
@@ -11,3 +11,12 @@
 # 2- It makes the software or any program easy to maintain.
 # 3- It increases the skills of the developer to think more by considering a object
 # 4 - We can reuse of code through inheritance
+
+# Question no. 3
+# Differentiate between function and method?
+
+# Function is creating a certain code with in the procedural oriented programming, which can do a certain work and we
+# call a function where we want with in the code...
+#
+# Methods are similar as of function. The main difference is that methods are the function use within the class which
+# can perform any action relating to the class attribute or anything else
